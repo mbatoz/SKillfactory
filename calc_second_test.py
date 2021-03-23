a2ac7a07986512ff8fe3b9e10fa82befb082d9b4
mbatoz <75219033+mbatoz@users.noreply.github.com>
2021-03-23 08:44:35 +0400

    Add files via upload

---
+++ calc_second_test.py
@@ -0,0 +1,19 @@
+
+from app.calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 6, 3) == 2
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 10, 7) == 3
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 10, 1) == 11
+
